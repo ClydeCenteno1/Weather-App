bb5c3cc2db419ba0b8dd5bcd03402c4023d6dfd0
ClydeCenteno1 <https://github.com/ClydeCenteno1>
2026-07-11 04:25:30 -0400

    Remove .gitignore and config.example.js; hardcode API key in app.js

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const weatherApi = async (city) => {
         const config = {
             params: {
                 q: city,
-                appId: WEATHER_API_KEY,
+                appId: "ac2b85cb8d99b66d9be80cf7a6523696",
                 units: "metric"
             }
         }
--- config.example.js
+++
@@ -1,2 +0,0 @@
-// Copy this file to config.js and add your OpenWeather API key.
-const WEATHER_API_KEY = "YOUR_OPENWEATHER_API_KEY_HERE";
